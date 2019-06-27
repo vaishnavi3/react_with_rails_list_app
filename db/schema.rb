0cb0cfd2dba2415aeafde0e8a82cb27802cd73a8
Vaishnavi <vaishnavi.bhaarde@blimp.co.in>
2019-06-27 14:40:07 +0530

    scaffold list, item, list-item

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_27_090322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.integer "type"
+    t.string "name"
+    t.text "excerpt"
+    t.text "description"
+    t.string "url"
+    t.integer "upvotes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "list_items", force: :cascade do |t|
+    t.bigint "list_id"
+    t.bigint "item_id"
+    t.text "description"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_list_items_on_item_id"
+    t.index ["list_id"], name: "index_list_items_on_list_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.text "excerpt"
+    t.text "description"
+    t.integer "upvotes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "list_items", "items"
+  add_foreign_key "list_items", "lists"
 end
